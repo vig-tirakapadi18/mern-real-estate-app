cbc26257ba0e588c8543a5c2ad6d69012538691e
vig-titakapadi18 <vighneshvt2000@gmail.com>
2023-11-19 19:50:59 +0530

    created update listing api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -31,3 +31,23 @@ exports.deleteListing = async (req, res, next) => {
                 next(error);
         }
 }
+
+exports.updateListing = async (req, res, next) => {
+        const listing = await Listing.findById(req.params.id);
+
+        if (!listing) return next(errorHandler(404, "Listing does not exist!"));
+
+        if (req.user.id !== listing.userRef)
+                return next(errorHandler(401, "Y0u are not authorized to update this listing!"));
+
+        try {
+                const updatedListing = await Listing.findByIdAndUpdate(
+                        req.params.id,
+                        req.body,
+                        { new: true }
+                );
+                res.status(200).json(updatedListing);
+        } catch (error) {
+                next(error);
+        }
+}
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -6,6 +6,7 @@ const { verifyToken } = require("../utils/verifyUser");
 const router = express.Router();
 
 router.post("/create", verifyToken, listingController.createListing);
-router.delete("/delete/:id", verifyToken, listingController.deleteListing)
+router.delete("/delete/:id", verifyToken, listingController.deleteListing);
+router.post("/update/:id", verifyToken, listingController.updateListing);
 
 module.exports = router;
